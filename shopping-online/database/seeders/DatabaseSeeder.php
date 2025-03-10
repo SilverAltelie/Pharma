e5d999aa4cddc22736cf5f9a78904f9d05f4b544
Hoang First <Hoang First>
2025-03-11 06:12:07 +0700

    new

--- shopping-online/database/seeders/DatabaseSeeder.php
+++ shopping-online/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Tạo 10 user giả lập
+        // User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Tạo dữ liệu cho bảng categories
+        Category::insert([
+            ['id' => 1, 'name' => 'Thuốc giảm đau', 'description' => 'Morphine các loại', 'status' => '1'],
+            ['id' => 2, 'name' => 'Kháng sinh', 'description' => 'Phòng ngừa các loại bệnh', 'status' => '1'],
+            ['id' => 11, 'name' => 'Paracetamol', 'description' => 'Thuốc đau đầu', 'status' => '1', 'parent_id' => 1], 
+            ['id' => 12, 'name' => 'Ibuprofen', 'description' => 'Thuốc giảm đau', 'status' => '0', 'parent_id' => 1],
+            ['id' => 21, 'name' => 'Amoxicillin', 'description' => 'Kháng sinh loại A', 'status' => '0', 'parent_id' => 2],
+            ['id' => 22, 'name' => 'Azithromycin', 'description' => 'Kháng sinh loại B', 'status' => '1', 'parent_id' => 2],
+        ]);
     }
 }
